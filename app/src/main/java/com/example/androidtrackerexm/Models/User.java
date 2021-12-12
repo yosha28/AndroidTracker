b9165df73a593f57849e4d6a5c54da1249dce382
yosha28 <56972122+yosha28@users.noreply.github.com>
2021-12-12 23:17:25 +0200

    test

--- app/src/main/java/com/example/androidtrackerexm/Models/User.java
+++ app/src/main/java/com/example/androidtrackerexm/Models/User.java
@@ -28,21 +28,7 @@ public class User {
         this.id = id;
     }
 
-    public String getEmail() {
-        return email;
-    }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
 
 }
